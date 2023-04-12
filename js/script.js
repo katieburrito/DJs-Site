89a5be881630ecad46e412ac36211f30cd3b168f
KatieB4 <61069568+KatieB4@users.noreply.github.com>
2023-04-12 10:33:54 -0400

    Fix broken external links
    Add if statement to smooth scroll js for in-page links to prevent its application to external links. The smooth scroll required preventing the default behavior of links, which when applied to all links (internal and external), was preventing the external links from opening.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const currentYear = new Date().getFullYear();
 const copyrightDate = document.querySelector(".year");
 copyrightDate.textContent = currentYear;
 
-
 // Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
@@ -11,34 +10,77 @@ const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
-})
+});
 
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
+  if (link.getAttribute("href").contains("#")) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const href = link.getAttribute("href");
 
-    //Scroll back to top
-    if (href === "#") {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      })
-    }
-    //Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({
-        behavior: "smooth"
-      });
-    }
-    // Close mobile navigation
-    if (link.classList.contains("main-nav-link")) {
-      headerEl.classList.toggle("nav-open");
-    }
-  });
+      //Scroll back to top
+      if (href === "#") {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+      //Scroll to other links
+      if (href !== "#" && href.startsWith("#")) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+      // Close mobile navigation
+      if (link.classList.contains("nav-item")) {
+        headerEl.classList.toggle("nav-open");
+      }
+    });
+  }
 });
+
+// STICKY NAVIGATION
+const sectionHero = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-120px",
+  }
+);
+observer.observe(sectionHero);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
